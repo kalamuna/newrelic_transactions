39efb64908a6a2b2cdba3eaec874cd67928c92f7
Kelly <kelly@kalamuna.com>
2022-04-08 11:40:30 -0700

    TECH-271: Functionality to apply config from config form

--- src/EventSubscriber/EventSubscriber.php
+++ src/EventSubscriber/EventSubscriber.php
@@ -10,11 +10,40 @@ namespace Drupal\newrelic_transactions\EventSubscriber;
 use Symfony\Component\HttpKernel\KernelEvents;
 use Symfony\Component\HttpKernel\Event\RequestEvent;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
 
 /**
  * Event subscriber that responds to page requests.
  */
 class EventSubscriber implements EventSubscriberInterface {
+
+  /**
+   * Config Factory for loading config.
+   *
+   * @var \Drupal\Core\Config\ConfigFactoryInterface
+   */
+  protected $config_factory;
+
+  /**
+   * The entity type manager.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
+   * Constructs a \Drupal\system\ConfigFormBase object.
+   *
+   * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
+   *   The factory for configuration objects.
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
+   *   The entity type manager.
+   */
+  public function __construct(ConfigFactoryInterface $config_factory, EntityTypeManagerInterface $entity_type_manager) {
+    $this->config_factory = $config_factory;
+    $this->entityTypeManager = $entity_type_manager;
+  }
 
   /**
    * {@inheritdoc}
@@ -29,9 +58,11 @@ class EventSubscriber implements EventSubscriberInterface {
    * Specify the name of this transation in New Relic based on routing and user info.
    */
   public function nameTransaction(RequestEvent $event) {
-
     // Only change New Relic data if New Relic is actually enabled.
     if (!extension_loaded('newrelic')) return;
+
+    // Config object.
+    $config = $this->config_factory->get('newrelic_transactions.config');
 
     // We are going to use the router path to name transactions.
     $route_match = \Drupal::routeMatch();
@@ -45,8 +76,28 @@ class EventSubscriber implements EventSubscriberInterface {
       }
     }
 
+    // Load roles for role order.
+    $roles = $this->entityTypeManager->getStorage('user_role')->loadMultiple();
+    $role_ids = array_keys($roles);
+    $roles = array_combine($role_ids, $role_ids);
+
+    // Roles that can be passed to New Relic.
+    $transaction_roles = $config->get('transaction_roles');
+
+    // Filter roles by transaction roles from config.
+    $roles = array_filter($roles, function($role) use ($transaction_roles) {
+      return $transaction_roles[$role] ?? FALSE;
+    });
+
     // We are going to prepend a role to the transaction name, since that can greatly affect performance.
     $user_roles = \Drupal::currentUser()->getRoles();
+
+    // Filter roles by what is set in config.
+    $user_roles = array_filter($roles, function($role) use ($user_roles) {
+      return in_array($role, $user_roles);
+    });
+    $user_roles = array_values($user_roles);
+
     // Select the highest-weighted role for the current user. (This should probably become configurable.)
     $transaction_role = array_pop($user_roles);
 
@@ -58,17 +109,23 @@ class EventSubscriber implements EventSubscriberInterface {
    * Specify the name of this transation in New Relic based on routing and user info.
    */
   public function addAttributes(RequestEvent $event) {
-
     // Only change New Relic data if New Relic is actually enabled.
     if (!extension_loaded('newrelic')) return;
 
+    // Config object.
+    $config = $this->config_factory->get('newrelic_transactions.config');
+
+    // Get user_data config.
+    $user_data = $config->get('user_data');
+
     // Track some data about the current user so we can identify who is having trouble.
     $user = \Drupal::currentUser();
-    newrelic_add_custom_parameter('user_id', $user->id());
-    newrelic_add_custom_parameter('user_roles', implode(', ', $user->getRoles()));
 
-    // Not sharing username and mail by default to avoid data retention and privacy issues.
-    //newrelic_add_custom_parameter('user_name', $user->getAccountName());
-    //newrelic_add_custom_parameter('user_mail', $user->getEmail());
+    if ($user_data['id']) {
+      newrelic_add_custom_parameter('user_id', $user->id());
+    }
+    if ($user_data['roles']) {
+      newrelic_add_custom_parameter('user_roles', implode(', ', $user->getRoles()));
+    }
   }
 }
